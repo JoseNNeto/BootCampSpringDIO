73ed3e9e875ec2d899c3cbb815df324d08f4a1da
José Neto <jnan@discente.ifpe.edu.br>
2024-08-28 16:26:31 -0300

    Application Properties

---
+++ application-properties/src/test/java/dio/application_properties/ApplicationPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.application_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
