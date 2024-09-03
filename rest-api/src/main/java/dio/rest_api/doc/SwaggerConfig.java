2b9c2b0472af9474a3b7603a76d22065c0763c04
José Neto <jnan@discente.ifpe.edu.br>
2024-09-03 17:05:10 -0300

    Add Swagger

---
+++ rest-api/src/main/java/dio/rest_api/doc/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package dio.rest_api.doc;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Title - Rest API")
+                        .description("API exemplo de uso de Springboot REST API")
+                        .version("1.0")
+                        .termsOfService("Termo de uso: Open Source")
+                        .license(new io.swagger.v3.oas.models.info.License().name("Licença - Sua Empresa").url("http://www.seusite.com.br"))
+                );
+    }
+
+    
+}
